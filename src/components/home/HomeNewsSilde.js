5dd5623297275401d669438e334221c183da6d83
deerschen <deerschen@163.com>
2017-08-18 20:25:54 +0800

    解决样式问题

--- src/components/home/HomeNewsSilde.js
+++ src/components/home/HomeNewsSilde.js
@@ -1,9 +1,13 @@
 import React,{ Component } from "react"
+import { Tabs } from "antd"
 
+const TabPane = Tabs.TabPane 
 export default class HomeNewsSlide extends Component{
-
 	render(){
-		return <div></div>
+		return( 
+			<div className="home-news-slide">
+		
+			</div>
+		)
 	}
-
 } 
